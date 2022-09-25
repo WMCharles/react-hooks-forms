33b6b41f32c7e23e2a9709dcfcc0269106000cb7
WMCharles <charles.wafula@student.moringaschool.com>
2022-09-25 11:25:06 +0300

    add DisplayData Component

---
+++ src/components/DisplayData.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function DisplayData(props) {
+    return (
+        <div>
+            <h2>Hello {props.firstName} {props.lastName}</h2>
+        </div>
+    )
+    }
